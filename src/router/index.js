b55047bad1a4557acf278a548f6b6996234ca1eb
Vinícius Risso <viniciusorisso@gmail.com>
2023-05-10 23:21:45 -0300

    feat: snake game single player done

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+// import HomeView from "../views/HomeView.vue";
+import FirstPageVue from "@/views/FirstPage.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: FirstPageVue,
   },
 ];
 
